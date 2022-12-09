9cfadbccdbd8a5268edaaa27df4902a8ab5f0745
HydroOx1d <n-esenov44@mail.ru>
2022-12-09 11:15:25 +0300

    coorect links

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
-
-import "./index.scss";
 import { ThemeProvider } from "@mui/material";
+import { BrowserRouter } from 'react-router-dom'
+import "./index.scss";
+import App from "./App";
+
 import { theme } from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -13,7 +14,9 @@ root.render(
   <React.StrictMode>
     <CssBaseline />
     <ThemeProvider theme={theme}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
