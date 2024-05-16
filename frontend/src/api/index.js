d69f98861eeed2fbf2eb25a8d9fa5c4f24f058c9
HydroOx1d <escoreze1773@gmail.com>
2024-05-16 20:07:05 +0600

    update api

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://my-blog-4i9v.onrender.com/",
 })
 
 instance.interceptors.request.use((config) => {
